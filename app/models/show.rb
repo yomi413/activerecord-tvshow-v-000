fc4c4d37e3d849d49116f61d61035f15b41b2c71
Yomaira Escano <yjescano@gmail.com>
2018-01-07 04:26:08 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,19 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
   end
 
+  def self.lowest_rating
+  end
+
+  def self.least_popular_show
+  end
+
+  def self.ratings_sum
+  end
+
+  def self.popular_shows
+  end
+
+  def self.shows_by_alphabetical_order
+  end
+
 end
