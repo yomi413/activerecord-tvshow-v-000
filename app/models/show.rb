16aaf8a9e5bc87b0b35b00c6e12dc7418840299a
Yomaira Escano <yjescano@gmail.com>
2018-01-07 04:52:57 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,8 +6,8 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     # binding.pry
-    show = self.where("rating = #{self.highest_rating}")
-    show.first
+    show = self.where("rating = #{self.highest_rating}").first
+    # show.first
   end
 
   def self.lowest_rating
