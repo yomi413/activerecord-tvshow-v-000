c25df0fb3578bff41bcebfd388138945821f62f3
Yomaira Escano <yjescano@gmail.com>
2018-01-07 03:34:49 +0000

    automatically backed up by learn

--- app/shows_controller.rb
+++ app/shows_controller.rb
@@ -1,11 +1,12 @@
 # class Show
 
-def create(attributes)
-  show = Show.create(attributes)
+# def create(attributes)
+#   show = Show.create(attributes)
+#   # binding.pry
+# end
+
+def highest_rating
   binding.pry
+  Show.maximum(:rating)
 end
-  # def highest_rating
-    # binding.pry
-    # Show.maximum(:rating)
-  # end
 # end
